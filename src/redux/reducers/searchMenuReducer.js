91b4db43104a008d0b5fa983ba95c5ef10514aa0
BetulKilincaslan <82607026+BetulKilincaslan@users.noreply.github.com>
2021-08-26 16:28:49 +0300

    Comment line deleted.

--- src/redux/reducers/searchMenuReducer.js
+++ src/redux/reducers/searchMenuReducer.js
@@ -1,7 +1,6 @@
 import * as actionTypes from "../actions/actionTypes";
 import initialState from "./initialState";
 
-// REDUCER AKSİYONA GÖRE STATE BELİRLER. YANİ STATE İ BURDAN KONTROL EDİYORUZ SET. REDUCER İÇİNDE STATE YÖNETİMİ YAPILIR.
 export default function searchMenuReducer(
   state = initialState.searchQuery,
   action
