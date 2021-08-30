6bc82507da7357d78bf746574f2bd3c56b56f9ba
BetulKilincaslan <82607026+BetulKilincaslan@users.noreply.github.com>
2021-08-30 18:26:52 +0300

    Added to change menu item quantity with buttons.

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,21 +1,40 @@
 import { MdDeleteForever } from "react-icons/md";
+import { FiMinus, FiPlus } from "react-icons/fi";
 import alertify from "alertifyjs";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as cartActions from "../../redux/actions/cartActions";
 
 const CartItem = ({ cartItem, actions }) => {
+  // **********  Change menu item quantity ********** //
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
+  useEffect(() => {
+    actions.addjustQuantity(cartItem.menuItem.id, quantity);
+  }, [quantity, cartItem.menuItem.id, actions]);
+
+  const increaseQuantity = () => {
+    setQuantity(parseInt(quantity + 1));
+    actions.addjustQuantity(cartItem.menuItem.id, quantity);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(parseInt(quantity - 1));
+    }
+    actions.addjustQuantity(cartItem.menuItem.id, quantity);
+  };
+
+  const onChangeHandler = (e) => {
+    setQuantity(e.target.value);
+    actions.addjustQuantity(cartItem.menuItem.id, e.target.value);
+  };
+
+  // **********  Remove menu item from cart ********** //
   const removeFromCart = (menuItem) => {
     actions.removeFromCart(menuItem);
     alertify.error(menuItem.name + " removed from cart", 2);
-  };
-
-  const [inputChange, setInputChange] = useState(cartItem.quantity);
-
-  const onChangeHandler = (e) => {
-    setInputChange(e.target.value);
-    actions.addjustQuantity(cartItem.menuItem.id, e.target.value);
   };
 
   return (
@@ -32,19 +51,27 @@ const CartItem = ({ cartItem, actions }) => {
         />
         <div className="flex-grow flex flex-col gap-2 sm:flex-row items-center justify-center sm:justify-between">
           <p className="sm:pl-4 sm:w-52">{cartItem.menuItem.name}</p>
-          <div className="flex gap-4 items-center justify-center">
-            <label htmlFor="quantity" className="text-sm">
-              Quantity :
-            </label>
+          <div className="flex items-center justify-center gap-2">
+            <span
+              onClick={() => decreaseQuantity()}
+              className="bg-yellow-400 px-1 py-2 rounded-full cursor-pointer"
+            >
+              <FiMinus />
+            </span>
             <input
-              min="1"
-              type="number"
+              type="text"
               id="quantity"
               name="quantity"
-              value={inputChange}
+              value={quantity}
               onChange={onChangeHandler}
-              className="w-16 p-2 bg-yellow-400 rounded-md opacity-90 outline-none"
+              className="w-12 p-2 bg-yellow-200 rounded-md opacity-90 outline-none"
             ></input>
+            <span
+              onClick={() => increaseQuantity()}
+              className="bg-yellow-400 px-1 py-2 rounded-full cursor-pointer"
+            >
+              <FiPlus />
+            </span>
           </div>
           <p className="tracking-widest text-sm">
             ${cartItem.menuItem.price * cartItem.quantity}
